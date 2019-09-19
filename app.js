e016356e8a9f0278813c173e6bbf13dc2e80fa6e
ames <amyabdelhalim@Amys-MacBook-Air.local>
2019-09-19 14:12:05 -0700

    working code

--- app.js
+++ app.js
@@ -1,57 +1,96 @@
 $(document).ready(function () {
+  var topicsarray = ["dog", "wolf", "giraffe", "bird"]
 
-  $("#sumbit").click(function){
+  $("#submit").on('click', function () {
+    event.preventDefault()
+    var val = $("#textbox").val()
+    $("#textbox").val("")
+
+    if (val) {
+      topicsarray.push(val)
+    }
+    renderbuttons()
     console.log("Submit")
-  }
+  })
 
-  var url = 'https://api.giphy.com/v1/gifs/search?q=${searchTopic}&api_key=oGYgispO8f1ljs4yIM0McQJsr1S8Iysi` 
-  var topicsarray = ["cat];
-$button.addClass("gify")
-    function renderbuttons() {
-      $("#animal-buttons").empty();
+  $(document).on("click", "button.gify", function () {
+    //this
+    var searchTopic = $(this).attr("cali")
+    console.log(searchTopic)
+    var url = `https://api.giphy.com/v1/gifs/search?q=${searchTopic}&api_key=oGYgispO8f1ljs4yIM0McQJsr1S8Iysi`
 
-      for (var i = 0; i < topicsarray.length, I++) {
-        var $button = $('<button>');
-
-        $button.attr("cali", topicsarray[i]);
-        $button.text(topicsarray[i]);
-        $("#animal-buttons").append($button)
-      }
-    } renderbuttons();
-
-  $.ajax({
-    URL,
-    method: "Get"
-      )
-}
-  .then(respone) = > {
-    console.log(response)
+    $.ajax({
+      url: url,
+      method: "get"
+    })
+      .then(response => {
+        console.log(response)
         $('#imageblock').empty();
-  }
-      var TopicIndex = TopicArray.indexof(searchtopic)
+        var results = response.data;
+        if (results.length === 0) {
+          var TopicIndex = topicsarray.indexOf(searchTopic)
+          topicsarray.splice(TopicIndex, 1);
+          alert("search not found, try again")
+          renderbuttons();
+          return;
+        }
+        for (let i = 0; i < results.length; i++) {
+          var $div = $('<div>');
+          var $img = $('<img>');
+          $img.attr('src', results[i].images.fixed_height_still.url);
+          $img.attr('data-animate', results[i].images.fixed_height.url);
+          $img.attr('data-still', results[i].images.fixed_height_still.url);
+          $img.attr('data-is-still', "yes");
 
-var results = response.data;
-if (results.length === 0) {
-  topicsarray.splice(TopicIndex, 1);
-  alert("search not found, try again")
+          $img.addClass("img")
+          var $p = $("<p>")
+          $div.append($p)
+          $div.append($img)
+
+          $('#image-block').append($div);
+        }
+
+      })
+  })
+
+  $(document).on("click", "img.img", function () {
+    //this
+    var isStill = $(this).attr("data-is-still")
+    if (isStill === "yes") {
+      $(this).attr("src", $(this).attr("data-animate"))
+      $(this).attr("data-is-still", "no")
+    } else {
+      $(this).attr("src", $(this).attr("data-still"))
+      $(this).attr("data-is-still", "yes")
+    }
+  })
+
+
+
+  function renderbuttons() {
+    $("#animal-buttons").empty();
+
+    for (var i = 0; i < topicsarray.length; i++) {
+      var $button = $('<button>');
+
+      $button.attr("cali", topicsarray[i]);
+      $button.text(topicsarray[i]);
+      $button.addClass("gify")
+      $("#animal-buttons").append($button)
+    }
+  }
+
   renderbuttons();
-  return ;
-}
-    ]
 
-  for (let i = 0; i< results.length, i++) {
-    var $div = $('<div>');
-    var $img = $('<img>');
-    $img.attr('src, results[i].images.fixed_height_still.url');
-     $img.addClass("img")
 
-     $div.append($p)
-     $div.append($img)
 
-     $('#imagesBlock').append($div);
-  }
+  //     console.log(response)
+  //   }
+
+  //     ]
 
 
 
 
-)}
+
+})
